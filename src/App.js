10f4c30f90b7c7c67c2a8fcf0777e24d93867f65
Sai Bingi <saibingi1208@gmail.com>
2022-05-22 12:03:41 +0000

    Registration form Solution

---
+++ src/App.js
@@ -0,0 +1,8 @@
+// Published URL ==> https://regstrationform.ccbp.tech
+
+import RegistrationForm from './components/RegistrationForm'
+import './App.css'
+
+const App = () => <RegistrationForm />
+
+export default App
